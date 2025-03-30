4e4ca5ede5d76fc3c1a99616ed0d3b07f16b00a4
Tushar <tkpathak2505@gmail.com>
2025-03-30 13:59:30 +0530

    Updated commit message

--- src/test/java/com/scm/scm20/ApplicationTests.java
+++ src/test/java/com/scm/scm20/ApplicationTests.java
@@ -1,15 +1,10 @@
 package com.scm.scm20;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.scm.scm20.services.EmailService;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 }
